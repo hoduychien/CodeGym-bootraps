888e71fe4a6ae652ea6bc113762e1bc79e0cce74
ChienDuy <chienduy2020@gmail.com>
2022-08-19 20:38:26 +0700

    done

--- js/app.js
+++ js/app.js
@@ -3,3 +3,25 @@ window.addEventListener("scroll", function () {
   header.classList.toggle("header-sticky", window.scrollY > 0);
 });
 
+let nCount = (selector) => {
+  $(selector).each(function () {
+    $(this).animate({
+      Counter: $(this).text()
+    }, {
+      duration: 4000,
+      easing: 'swing',
+      step: function (value) {
+        $(this).text(Math.ceil(value))
+      }
+    })
+  });
+};
+
+let position = 0;
+$(window).scroll(function () {
+  let oTop = $('.feature-item').offset().top - window.innerHeight;
+  if (position == 0 && $(window).scrollTop() >= oTop) {
+    position++;
+    nCount(".number")
+  }
+})
